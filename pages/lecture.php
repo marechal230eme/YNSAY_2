3d07ae5806e089b5cb32986cbd2352490e22c79a
Antoine PARANT <antoine.parant83@gmail.com>
2017-01-30 18:11:58 +0100

    Twig et correction de chemin de fichier
    Ajout de Twig et correction de chemin de fichier

--- pages/lecture.php
+++ pages/lecture.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        include('../twig/t_twig.php');
 
+        $template = $twig->loadTemplate('t_index.twig');
+            echo $template->render(array(
+                'img_profil' => '../images/avatar.png',
+            ));
+            
+    ?>
+    </body>
+</html>
