49109442d12cccac26aaa1cfffac686720f4a057
Antoine PARANT <antoine.parant83@gmail.com>
2017-02-24 17:40:11 +0100

    debut page lecture CSS
    ajout navbar et style

---
+++ includes/i_navbar_lecture.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<link href="../css/i_navbar_lecture.css" rel="stylesheet" type="text/css"/>
+
+ 
+<ul class="topnav">
+    <a href="../pages/lecture.php"><img class="logo" src="../images/logo.png"></a>
+    <li class="right">
+        <a href="../pages/ecriture.php"><button class="btn waves-effect waves-light btn-large orange accent-4 " type="submit" name="valider">Envoyer
+            <i class="material-icons right">email</i>
+        </button></a>
+    </li>
+    <p class="titre">Exprimez-vous</p>
+</ul>
+<a href="../pages/ecriture.php"></a>
