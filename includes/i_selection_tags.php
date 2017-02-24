a28697c3764d2d9b51de6da756082efddfd3780e
marechal230eme <marechal230eme@gmail.com>
2017-02-24 14:39:03 +0100

    petite modif

--- includes/i_selection_tags.php
+++ includes/i_selection_tags.php
@@ -9,32 +9,24 @@
 $id_tag;
 $nom_tag;
 $description_tag;
-
-$valeur_id ; 
+$valeur_id;
 
 include '../objets/o_requete.php';
-
 $objet = new o_requete();
-
 $objet->recupere_tag($id_tag, $nom_tag, $description_tag);
 
 /*
-//voir ce que  les valeurs contiennent quand on débug 
-echo "id_tag";
-var_dump($id_tag);
-echo "nom_tag";
-var_dump($nom_tag);
-echo "description_tag";
-var_dump($description_tag);
-*/
+  //voir ce que  les valeurs contiennent quand on débug
+  echo "id_tag";
+  var_dump($id_tag);
+  echo "nom_tag";
+  var_dump($nom_tag);
+  echo "description_tag";
+  var_dump($description_tag);
+ */
 
 $i = 0;                                                                                                           //variable d'incrémentation
 $description[$i] = $description_tag[0];                                                                           // initialisation de la variable desription qui contiendra une descripton_de_tag par tag
-
-
-
-
-
 
 foreach ($description_tag as $traitement) {                                                                       //**pout chaque ligne de description de description tag on fait :
     if (((strnatcmp($description[$i], $traitement)) < 0) || ((strnatcmp($description[$i], $traitement)) > 0)) {   //* comparaison des 2 chaine 2 caractère pour eviter les doublons 
@@ -44,9 +36,10 @@ foreach ($description_tag as $traitement) {                                     
 }
 
 /*
-echo 'description_tag apres traitement : ';
-var_dump($description);
-*/
+  echo 'description_tag apres traitement : ';
+  var_dump($description);
+ */
+
 $i = 0;                                                 // valeur remise à 0 pour l'incrémentation   
 
 foreach ($id_tag as $construction) {                    //** pour le nombre d'id présent on associe dans un tableau 2d : 
@@ -57,11 +50,9 @@ foreach ($id_tag as $construction) {                    //** pour le nombre d'id
 }
 
 /*
-echo 'valeur du tableau des case';
-var_dump($case_description);
-*/
-
-
+  echo 'valeur du tableau des case';
+  var_dump($case_description);
+ */
 
 $i = 0;
 foreach ($description as $case) {
@@ -70,25 +61,31 @@ foreach ($description as $case) {
         <select multiple>
             <?php
             echo"<option value=\"\" disabled selected> $case </option>";
-
-
-
+            
             foreach ($case_description[0] as $ligne) {
 
                 if (((strnatcmp(($case_description[2][$i]), $case)) == 0)) {
-
-                    $valeur_id  = $case_description[0][$i] ; 
-                    echo"<option  id=$valeur_id name='".$idTag[$i]=$valeur_id."' value=$valeur_id  > " . ($case_description[1][$i]) ."  </option> ";
+                    
+                    $valeur_id = $case_description[0][$i];
+                    
+                    echo"<option   name='" . $idTag[$i] . "' value=$valeur_id  > " . ($case_description[1][$i]) . "  </option> ";
+                
+                   
                 }
                 $i++;
+                
+               
             }
-            $i=0; 
+            
+            $i = 0;
+            
             ?>
-        </select>     
+        </select>
+         
     </div>
     <?php
+     
 }
-
 
 /*
 echo 'ce qu il se passe dans les foreach ';
