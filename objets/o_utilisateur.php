8968135411e736f141b0c12be91c7cf456aadb36
aberenguer <aberenguer>
2017-02-24 16:39:54 +0100

    rajout de la méthode deconnexion()
    supprime les variables de session et détruit la session

--- objets/o_utilisateur.php
+++ objets/o_utilisateur.php
@@ -13,6 +13,7 @@ if (session_status() == PHP_SESSION_NONE) //on démarre une session si ce n'est 
 }
 
 class o_utilisateur {
+	const OK = "ok";
     const ERR_DOESNOTEXIST = "ce que vous recherchez n'existe pas.";
 	
 	public function recupere_pseudo() //fonction qui retourne le pseudo de l'utilisateur connecté, ou ERR_DOESNOTEXIST si il n'est pas connecté.
@@ -26,5 +27,12 @@ class o_utilisateur {
 			$pseudo_utilisateur = $_SESSION['pseudo'];
 			return $pseudo_utilisateur;
 		}
+	}
+	
+	public function deconnexion() //supprime toutes les variables de session et arrête la session
+	{
+		$_SESSION = array(); //on vire les variables de session
+		session_destroy(); //on arrête la session
+		return self::OK;
 	}
 }
