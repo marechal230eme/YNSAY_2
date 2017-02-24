f06591131aea2360fba921fbea8e0cc5eeb61a38
aberenguer <aberenguer>
2017-02-24 16:24:24 +0100

    méthode recupere_pseudo pour la classe o_utilisateur
    comme son nom l'indique elle retourne le pseudo de l'utilisateur
    connecté.

--- objets/o_utilisateur.php
+++ objets/o_utilisateur.php
@@ -7,6 +7,24 @@ Nom de l'objet : o_utilisateur
 Rôle : gère les méthodes associées aux utilisateurs
 */
 
+if (session_status() == PHP_SESSION_NONE) //on démarre une session si ce n'est pas déjà le cas
+{
+	session_start();
+}
+
 class o_utilisateur {
-    //put your code here
+    const ERR_DOESNOTEXIST = "ce que vous recherchez n'existe pas.";
+	
+	public function recupere_pseudo() //fonction qui retourne le pseudo de l'utilisateur connecté, ou ERR_DOESNOTEXIST si il n'est pas connecté.
+	{
+		if (!isset($_SESSION['pseudo']))
+		{
+			return self::ERR_DOESNOTEXIST;
+		}
+		else
+		{
+			$pseudo_utilisateur = $_SESSION['pseudo'];
+			return $pseudo_utilisateur;
+		}
+	}
 }
